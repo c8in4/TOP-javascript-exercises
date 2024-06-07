51c21fa93637bf4bac59252ff8060a2e61a52d1b
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-06-08 08:06:13 +1200

    Solved exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,12 @@
-const removeFromArray = function() {
+const removeFromArray = function (array, ...nums) {
+    nums.forEach((num) => {
+        while (array.includes(num)) {
+            const index = array.indexOf(num);
+            array.splice(index, 1);
+        }
+    }
+    );
+    return array;
 };
 
 // Do not edit below this line
