22d7fef1da715b4d1c82e7d7c5c4c373dfaa895e
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-06-08 09:14:15 +1200

    Solved exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function (startNum, endNum) {
+    let sum = 0;
 
+    if (typeof (startNum) === "number"
+        && typeof (endNum) === "number"
+        && startNum >= 0
+        && endNum >= 0
+    ) {
+        if (startNum > endNum) {
+            const tempNum = startNum;
+            startNum = endNum;
+            endNum = tempNum;
+        }
+
+        for (let i = startNum; i <= endNum; i++) {
+            sum += i;
+        }
+        return sum;
+    } else {
+        return "ERROR";
+    }
 };
 
 // Do not edit below this line
