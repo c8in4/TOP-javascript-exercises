4a8cf6dca35cbdfe4e9f5e04363f85e1aacba508
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-06-16 04:11:10 +1200

    Solved exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,7 +1,7 @@
 const palindromes = function (str) {
     const newStr = str.replaceAll(/\W/g, "").toLowerCase();
     const reversedStr = newStr.split("").reverse().join("");
-    return (newStr == reversedStr) ? true : false;
+    return newStr == reversedStr
 };
 
 // Do not edit below this line
--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,11 @@
-const fibonacci = function() {
-
+const fibonacci = function (num) {
+    num = +num;
+    if (num < 0) return "OOPS";
+    let fibSeq = [0, 1];
+    for (let i = 0; i < num; i++) {
+        fibSeq.unshift(fibSeq[0] + fibSeq[1])
+    }
+    return fibSeq[0];
 };
 
 // Do not edit below this line
