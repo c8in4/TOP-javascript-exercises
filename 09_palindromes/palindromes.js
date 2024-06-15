3563e0d3590564247aeabcb7b03c03964a42af7f
c8in4 <106409156+c8in4@users.noreply.github.com>
2024-06-15 21:51:01 +1200

    Solved exercise 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,7 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    const newStr = str.replaceAll(/\W/g, "").toLowerCase();
+    const reversedStr = newStr.split("").reverse().join("");
+    return (newStr == reversedStr) ? true : false;
 };
 
 // Do not edit below this line
